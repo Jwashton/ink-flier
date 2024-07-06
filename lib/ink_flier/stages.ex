f0ed07943eb1254ca965a77104ae8558b4d61c40
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-06 00:28:54 -0400

    Add default catchall error for non-whitelisted Stage events

--- lib/ink_flier/stages.ex
+++ lib/ink_flier/stages.ex
@@ -1,3 +1,7 @@
 defmodule InkFlier.Stages do
+  defstruct stage: :adding_players
 
+  def new, do: struct!(__MODULE__)
+
+  def check(_t, _event), do: :error
 end
--- test/ink_flier/stages_test.exs
+++ test/ink_flier/stages_test.exs
@@ -2,4 +2,19 @@ defmodule InkFlierTest.Stages do
   use ExUnit.Case
 
   alias InkFlier.Stages
+
+  # adding_players
+  # locking_in_turns
+  # processing
+  #   -anyWinners?
+  #   -back to locking_in_turns
+  # game_over
+
+  test "Stage doesn't change if wrong action taken" do
+    stages = Stages.new
+    result = Stages.check(stages, :completely_wrong_event)
+
+    assert result == :error
+    assert stages.stage == :adding_players
+  end
 end
