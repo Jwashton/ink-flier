dbd5de89681428e5e14cd03eeee55a804da7307e
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-12 16:26:39 -0400

    Add starter HouseRules module with blank map as starter placeholder

---
+++ lib/ink_flier/house_rules.ex
@@ -0,0 +1,5 @@
+defmodule InkFlier.HouseRules do
+
+  # TODO This will have the default rules, by default, then can override others.
+  def new, do: %{}
+end
---
+++ test/ink_flier/house_rules_test.exs
@@ -0,0 +1,5 @@
+defmodule InkFlierTest.HouseRules do
+  use ExUnit.Case
+
+  alias InkFlier.HouseRules
+end
