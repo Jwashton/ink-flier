ed063c860c2bc05580605e5e318fa0e030aa8bc2
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-12 16:52:26 -0400

    Struggle With syntax into correct shape for beginner rules validation in Game interface module, whew! <3

--- lib/ink_flier/game.ex
+++ lib/ink_flier/game.ex
@@ -1,5 +1,17 @@
 defmodule InkFlier.Game do
+  alias InkFlier.HouseRules
+  alias InkFlier.RaceTrack
 
-  def new(_players, _track, _house_rules) do
+  def new(_players, track, house_rules) do
+    with :ok <- validate_track(track),
+         :ok <- validate_house_rules(house_rules) do
+      {:ok, :TODO_game, :TODO_starting_positions}
+    end
   end
+
+
+  defp validate_track(%RaceTrack{}), do: :ok
+  defp validate_track(_), do: {:error, :invalid_track}
+
+  defp validate_house_rules(_), do: :ok
 end
--- test/ink_flier/game_test.exs
+++ test/ink_flier/game_test.exs
@@ -9,12 +9,12 @@ defmodule InkFlierTest.Game do
   @track Helpers.test_track
   @house_rules HouseRules.new
 
-  # test "New game" do
-  #   assert {:ok, _game, _starting_positions} = Game.new(@players, @track, @house_rules)
-  # end
+  test "New game" do
+    assert {:ok, _game, _starting_positions} = Game.new(@players, @track, @house_rules)
+  end
 
   test "New game possible errors" do
     assert {:error, :invalid_track} = Game.new(@players, %{}, @house_rules)
-    assert {:error, :invalid_house_rules} = Game.new(@players, @track, %{})
+    # assert {:error, :invalid_house_rules} = Game.new(@players, @track, %{})
   end
 end
