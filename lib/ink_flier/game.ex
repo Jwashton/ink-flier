2a5a719be6e0a2d290e144ef41231a4a74ae8c0d
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-16 22:38:27 -0400

    Continue delegating most of the stuff Game was doing down to Board; Change Board.new to only return {:ok, board} instead of {:ok, board, starting_positions}
    (interface can just ask the returned `board` for current positions)

--- lib/ink_flier/game.ex
+++ lib/ink_flier/game.ex
@@ -7,21 +7,22 @@ defmodule InkFlier.Game do
 
   typedstruct enforce: true do
     field :board, Board.t
+    # field :players...
   end
 
   def new(players, track, house_rules \\ HouseRules.new) do
     with :ok <- validate_track(track),
          :ok <- validate_house_rules(house_rules) do
 
-      board =
-        track
-        |> Board.new
-        |> Board.start(players, house_rules.random_pole_position?)
-
-      # {:ok, :TODO_game, starting_positions}
-      {:ok, :TODO_game, Board.current_positions(board)}
+      track
+      |> Board.new
+      |> Board.start(players, house_rules.random_pole_position?)
+      |> then(& struct!(__MODULE__, board: &1))
+      |> ok
     end
   end
+
+  def current_positions(t), do: Board.current_positions(t.board)
 
 
   defp validate_track(%RaceTrack{}), do: :ok
@@ -29,4 +30,6 @@ defmodule InkFlier.Game do
 
   defp validate_house_rules(%HouseRules{}), do: :ok
   defp validate_house_rules(_), do: {:error, :invalid_house_rules}
+
+  defp ok(t), do: {:ok, t}
 end
--- test/ink_flier/game_test.exs
+++ test/ink_flier/game_test.exs
@@ -6,22 +6,11 @@ defmodule InkFlierTest.Game do
 
   @players ~w(a b c)a
   @track Helpers.test_track
-  @expected_starting_positions %{
-    a: {0,0},
-    b: {-1,-1},
-    c: {-2,-2},
-  }
 
-  test "New game returns starting position" do
-    assert {:ok, _game, starting_positions} = Game.new(@players, @track)
-    assert starting_positions == @expected_starting_positions
+  test "New game will hold Board and things like starting positions" do
+    assert {:ok, game} = Game.new(@players, @track)
+    assert %{b: {-1,-1}} = Game.current_positions(game)
   end
-
-  # test "New game stores, at the very least, current positions" do
-  #   {:ok, game, _starting_positions} = Game.new(@players, @track)
-  #   assert game |> Game.positions == @expected_starting_positions
-  #   # TODO next ah, wait, this is where we start to use Board. Game was just for rules checks and overall interface
-  # end
 
   test "New game possible errors" do
     assert {:error, :invalid_track} = Game.new(@players, %{})
