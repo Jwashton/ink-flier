d96d039be65d668a4fa80ae0aa25ae439d498269
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-17 16:54:47 -0400

    refactor: Shorten tests in LineTest

--- test/ink_flier/line_test.exs
+++ test/ink_flier/line_test.exs
@@ -7,31 +7,21 @@ defmodule InkFlierTest.Line do
     test "Clockwise" do
       # .a....b...
       # ........c.
-      a = {0,0}
-      b = {10,0}
-      c = {11,-1}
-
+      [a,b,c] = [{0,0}, {10,0}, {11,-1}]
       assert Line.orientation(a,b,c) == :clockwise
     end
 
     test "Counterclockwise" do
       # ....c.....
       # .a....b...
-      a = {0,0}
-      b = {10,0}
-      c = {8,1}
-
+      [a,b,c] = [{0,0}, {10,0}, {8,1}]
       assert Line.orientation(a,b,c) == :counterclockwise
     end
 
     test "Collinear" do
       # .a....b..c....
-      a = {0,0}
-      b = {10,0}
-      c = {15,0}
-
+      [a,b,c] = [{0,0}, {10,0}, {15,0}]
       assert Line.orientation(a,b,c) == :collinear
-
     end
   end
 end
