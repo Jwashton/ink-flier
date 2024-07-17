55bcf2c41b389ace5cc6a09d04a9155f2e48805e
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-17 16:34:32 -0400

    Add function to find orientation of ordered triplet of coordinates

--- lib/ink_flier/line.ex
+++ lib/ink_flier/line.ex
@@ -4,4 +4,20 @@ defmodule InkFlier.Line do
 
   All orientation and intersection code in this module adapted from [Check if two line segments intersect](https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect) article, by Ansh Riyal
   """
+
+  @doc """
+  Find the orientation of an ordered triplet of coordinates (p, q, r)
+
+  See [Orientation of 3 ordered points](https://www.geeksforgeeks.org/orientation-3-ordered-points/amp) for details of formula
+  """
+  @spec orientation(Coord.t, Coord.t, Coord.t) :: :clockwise | :counterclockwise | :collinear
+  def orientation({px, py}, {qx, qy}, {rx, ry}) do
+    val = (qy - py) * (rx - qx) - (qx - px) * (ry - qy)
+
+    cond do
+      val > 0 -> :clockwise
+      val < 0 -> :counterclockwise
+      val == 0 -> :collinear
+    end
+  end
 end
