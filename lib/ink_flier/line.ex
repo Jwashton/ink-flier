73d48b24763a3b97602daded055fb31829a61e70
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-18 01:00:28 -0400

    Add main Line intersect function. Still needs to be tested and refactored

--- lib/ink_flier/line.ex
+++ lib/ink_flier/line.ex
@@ -1,11 +1,50 @@
 defmodule InkFlier.Line do
   @moduledoc """
-  Functions for working with Lines and determining intersections
+  Functions for working with line segments and determining intersections
 
   Code in this module adapted from [Check if two line segments intersect](https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect) article, by Ansh Riyal
   """
 
   alias InkFlier.Coord
+
+  @type t :: {Coord.t, Coord.t}
+
+  @spec new(Coord.t, Coord.t) :: t
+  def new(p, q), do: {p, q}
+
+  @doc """
+  The main function which returns true if the line segments intersect
+  """
+  @spec intersect?(t, t) :: boolean
+  # TODO change p1 to px, etc
+  def intersect?({p1, q1}, {p2, q2}) do
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+
+    cond do
+      # General case
+      (o1 != o2) and (o3 != o4) -> true
+
+      # Special Cases
+
+      # p1, q1 and p2 are collinear and p2 lies on segment p1q1
+      (o1 == :collinear) and on_segment?(p1, p2, q1) -> true
+
+      # p1 , q1 and q2 are collinear and q2 lies on segment p1q1
+      (o2 == 0) and on_segment?(p1, q2, q1) -> true
+
+      # p2 , q2 and p1 are collinear and p1 lies on segment p2q2
+      (o3 == 0) and on_segment?(p2, p1, q2) -> true
+
+      # p2 , q2 and q1 are collinear and q1 lies on segment p2q2
+      (o4 == 0) and on_segment?(p2, q1, q2) -> true
+
+      true -> false
+    end
+
+  end
 
   @doc """
   Find the orientation of an ordered triplet of coordinates (p, q, r)
--- test/ink_flier/line_test.exs
+++ test/ink_flier/line_test.exs
@@ -21,4 +21,24 @@ defmodule InkFlierTest.Line do
     assert Line.on_segment?({0,0}, {5,0}, {10,0})
     refute Line.on_segment?({0,0}, {15,0}, {10,0})
   end
+
+  describe "intersect?/2" do
+    test "X-shape lines" do
+      assert Line.intersect?(
+        Line.new({10,0}, {0,10}),
+        Line.new({0,0}, {10,10}))
+    end
+
+    test "parallel lines" do
+      refute Line.intersect?(
+        Line.new({1,1}, {10,1}),
+        Line.new({1,2}, {10,2}))
+    end
+
+    test "2 unconnected segments on the same slope" do
+      refute Line.intersect?(
+        Line.new({-5,-5}, {0,0}),
+        Line.new({1,1}, {10,10}))
+    end
+  end
 end
