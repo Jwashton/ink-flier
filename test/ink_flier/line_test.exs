674c1df5e9e6bdbcca3ff494a096577a54549122
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-19 00:47:44 -0400

    Move main intersect?/2 test to top of file

--- test/ink_flier/line_test.exs
+++ test/ink_flier/line_test.exs
@@ -3,25 +3,6 @@ defmodule InkFlierTest.Line do
 
   alias InkFlier.Line
 
-  describe "Orientation" do
-    test "Clockwise" do
-      assert Line.orientation({0,0}, {10,0}, {11,-1}) == :clockwise
-    end
-
-    test "Counterclockwise" do
-      assert Line.orientation({0,0}, {10,0}, {8,1}) == :counterclockwise
-    end
-
-    test "Collinear" do
-      assert Line.orientation({0,0}, {10,0}, {15,0}) == :collinear
-    end
-  end
-
-  test "on_segment?/3" do
-    assert Line.on_segment?({0,0}, {5,0}, {10,0})
-    refute Line.on_segment?({0,0}, {15,0}, {10,0})
-  end
-
   describe "intersect?/2" do
     test "X-shape lines" do
       assert Line.intersect?(
@@ -40,5 +21,24 @@ defmodule InkFlierTest.Line do
         Line.new({-5,-5}, {0,0}),
         Line.new({1,1}, {10,10}))
     end
+  end
+
+  describe "Orientation" do
+    test "Clockwise" do
+      assert Line.orientation({0,0}, {10,0}, {11,-1}) == :clockwise
+    end
+
+    test "Counterclockwise" do
+      assert Line.orientation({0,0}, {10,0}, {8,1}) == :counterclockwise
+    end
+
+    test "Collinear" do
+      assert Line.orientation({0,0}, {10,0}, {15,0}) == :collinear
+    end
+  end
+
+  test "on_segment?/3" do
+    assert Line.on_segment?({0,0}, {5,0}, {10,0})
+    refute Line.on_segment?({0,0}, {15,0}, {10,0})
   end
 end
