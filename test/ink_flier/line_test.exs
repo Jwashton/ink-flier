dff74beabeb484e3dbdb47fd44be35a11a3c03f8
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-20 00:19:51 -0400

    Split intersect?/2 tests into "should return true" and "should return false" sections; Add a couple more on_segment?/3 tests hopefully confirming it always returns false when not collinear

--- test/ink_flier/line_test.exs
+++ test/ink_flier/line_test.exs
@@ -3,29 +3,11 @@ defmodule InkFlierTest.Line do
 
   alias InkFlier.Line
 
-  describe "intersect?/2" do
+  describe "intersect?/2 == true" do
     test "X-shape lines" do
       assert Line.intersect?(
         Line.new({10,0}, {0,10}),
         Line.new({0,0}, {10,10}))
-    end
-
-    test "parallel lines" do
-      refute Line.intersect?(
-        Line.new({1,1}, {10,1}),
-        Line.new({1,2}, {10,2}))
-    end
-
-    test "2 unconnected segments on the same slope" do
-      refute Line.intersect?(
-        Line.new({-5,-5}, {0,0}),
-        Line.new({1,1}, {10,10}))
-    end
-
-    test "2 connected segments on the same slope" do
-      assert Line.intersect?(
-        Line.new({0,0}, {10,0}),
-        Line.new({5,0}, {15,0}))
     end
 
     test "T-shape (touching right on end of line)" do
@@ -34,10 +16,30 @@ defmodule InkFlierTest.Line do
         Line.new({10,10}, {10,-10}))
     end
 
+    test "2 connected segments on the same slope" do
+      assert Line.intersect?(
+        Line.new({0,0}, {10,0}),
+        Line.new({5,0}, {15,0}))
+    end
+  end
+
+  describe "intersect?/2 == false" do
+    test "parallel lines" do
+      refute Line.intersect?(
+        Line.new({1,1}, {10,1}),
+        Line.new({1,2}, {10,2}))
+    end
+
     test "Unconnected but very close to line tip & diagonally hanging above it" do
       refute Line.intersect?(
         Line.new({0,0}, {10,0}),
         Line.new({9,11}, {11,9}))
+    end
+
+    test "2 unconnected segments on the same slope" do
+      refute Line.intersect?(
+        Line.new({-5,-5}, {0,0}),
+        Line.new({1,1}, {10,10}))
     end
   end
 
@@ -55,8 +57,15 @@ defmodule InkFlierTest.Line do
     end
   end
 
-  test "on_segment?/3" do
-    assert Line.on_segment?({0,0}, {5,0}, {10,0})
-    refute Line.on_segment?({0,0}, {15,0}, {10,0})
+  describe "on_segment?/3" do
+    test "Returns correct answer when collinear" do
+      assert Line.on_segment?({0,0}, {5,0}, {10,0})
+      refute Line.on_segment?({0,0}, {15,0}, {10,0})
+    end
+
+    test "Returns false automatically when not collinear" do
+      refute Line.on_segment?({0,0}, {5,1}, {10,0})
+      refute Line.on_segment?({0,0}, {99,99}, {10,0})
+    end
   end
 end
