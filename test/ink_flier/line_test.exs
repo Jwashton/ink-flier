d2c3418079283db4ebe4da938dbdf919a3d64c84
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-20 06:19:15 -0400

    Squash 3 tests into 1 since it makes sense here

--- test/ink_flier/line_test.exs
+++ test/ink_flier/line_test.exs
@@ -49,18 +49,10 @@ defmodule InkFlierTest.Line do
     end
   end
 
-  describe "Orientation" do
-    test "Clockwise" do
-      assert Line.orientation({0,0}, {10,0}, {11,-1}) == :clockwise
-    end
-
-    test "Counterclockwise" do
-      assert Line.orientation({0,0}, {10,0}, {8,1}) == :counterclockwise
-    end
-
-    test "Collinear" do
-      assert Line.orientation({0,0}, {10,0}, {15,0}) == :collinear
-    end
+  test "Orientation" do
+    assert Line.orientation({0,0}, {10,0}, {11,-1}) == :clockwise
+    assert Line.orientation({0,0}, {10,0}, {8,1}) == :counterclockwise
+    assert Line.orientation({0,0}, {10,0}, {15,0}) == :collinear
   end
 
   test "on_segment?/3" do
