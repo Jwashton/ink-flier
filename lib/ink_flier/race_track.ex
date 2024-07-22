2295651747e7647762698415af911ed20b25d6c1
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-22 01:05:58 -0400

    Implement RaceTrack.check_collision, still needs refactoring

--- lib/ink_flier/race_track.ex
+++ lib/ink_flier/race_track.ex
@@ -18,6 +18,7 @@ defmodule InkFlier.RaceTrack do
 
   alias InkFlier.RaceTrack.Obstacle
   alias InkFlier.Coord
+  alias InkFlier.Line
 
   typedstruct enforce: true do
     field :start, coord_list
@@ -30,26 +31,52 @@ defmodule InkFlier.RaceTrack do
   @type coord_list :: [Coord.t]
   @type line :: {Coord.t, Coord.t}
   @type obstacle :: Obstacle.t
+  @type collision_check :: :ok | {:collision, obstacle}
 
 
   @spec new(Keyword.t) :: t
   def new(attrs), do: struct!(__MODULE__, attrs)
 
-  @spec check_collision(t, Coord.t, Coord.t) :: :ok | {:collision, collision_object}
-  def check_collision(t, _a, _b) do
-    [t.inner_wall, t.outer_wall]
-    |> Enum.find(fn wall ->
-      _wall_lines = Enum.chunk_every(wall, 2, 1, :discard)
-    end)
   defdelegate new_obstacle(coord_list), to: Obstacle, as: :new
   defdelegate new_obstacle(coord_list, name), to: Obstacle, as: :new
 
-    # TODO hc
-    {:collision, :outer_wall}
+  @doc """
+  Check if the line between a car's previous & new position collides with any track obstacles
+  """
+  @spec check_collision(t, Line.t) :: collision_check
+  def check_collision(t, car_line) do
+    # NOTE Possible future optimization- This is a fair number of loops-in-loops; for each little line piece of
+    # each wall or obstacle, check for intersect
+    # Also, builds that list of line pieces from scratch with Enum.chunk_every
+    # If this runs slow, I can start by pre-building that Enum.chunk_every ONE time when track is made and storing it
+    # as extra data in the struct
+
+    collision_set =
+      t.obstacles
+      |> Enum.reduce(MapSet.new, fn obstacle, collision_set ->
+        wall_lines =
+          obstacle
+          |> Obstacle.coord_list
+          |> Enum.chunk_every(2, 1, :discard)
+          |> Enum.map(fn [p,q] -> Line.new(p,q) end)
+
+        if Enum.find(wall_lines, &Line.intersect?(&1, car_line)) do
+          MapSet.put(collision_set, Obstacle.name(obstacle))
+        else
+          collision_set
+        end
+      end)
+
+    if MapSet.size(collision_set) > 0 do
+      {:collision, collision_set}
+    else
+      :ok
+    end
   end
 
-  # @doc false
-  # def
+  @doc "See `check_collision/2`"
+  @spec check_collision(t, Coord.t, Coord.t) :: collision_check
+  def check_collision(t, p, q), do: check_collision(t, Line.new(p, q))
 
   def start(t), do: t.start
 end
--- test/ink_flier/race_track_test.exs
+++ test/ink_flier/race_track_test.exs
@@ -4,13 +4,11 @@ defmodule InkFlierTest.RaceTrack do
   alias InkFlierTest.Helpers
   alias InkFlier.RaceTrack
 
+  @t Helpers.test_track
+
   test "Check for collisions" do
-    t = Helpers.test_track
-
-    # assert RaceTrack.check_collision(t, :TODO) = :ok
-    assert RaceTrack.check_collision(t, {14,14}, {99,99}) == {:collision, :outer_wall}
+    assert RaceTrack.check_collision(@t, {11,0}, {11,1}) == :ok
+    assert RaceTrack.check_collision(@t, {14,14}, {99,99}) == {:collision, MapSet.new(["Outer Wall"])}
+    assert RaceTrack.check_collision(@t, {-99,-99}, {99,99}) == {:collision, MapSet.new(["Outer Wall", "Inner Wall"])}
   end
-
-  # # TODO does clipping the cornor on the exact point count? I think so
-  # assert RaceTrack.check_collision(t, {11,9}, {9,11}) = {:collision, :inner_wall}
 end
