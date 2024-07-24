ba251aa8ab77081e0db07b0505c0429c047c32aa
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-24 17:13:33 -0400

    Comment out most of Stages for now, going to try driving it from Game

--- lib/ink_flier/stages.ex
+++ lib/ink_flier/stages.ex
@@ -9,9 +9,9 @@ defmodule InkFlier.Stages do
   # TODO add all possible stages here to type after deciding the last few
   @type stage :: :todo
 
-  def new, do: struct!(__MODULE__)
+#   def new, do: struct!(__MODULE__)
 
-  def check(%{stage: :adding_players} = t, :add_player), do: {:ok, t}
+#   def check(%{stage: :adding_players} = t, :add_player), do: {:ok, t}
 
-  def check(_t, _event), do: :error
+#   def check(_t, _event), do: :error
 end
--- test/ink_flier/stages_test.exs
+++ test/ink_flier/stages_test.exs
@@ -1,7 +1,7 @@
 defmodule InkFlierTest.Stages do
   use ExUnit.Case
 
-  alias InkFlier.Stages
+  # alias InkFlier.Stages
 
   # - initial_setup
   #   - add_player
@@ -16,30 +16,30 @@ defmodule InkFlierTest.Stages do
   #   -back to locking_in_turns
   # game_over
 
-  test "Stage doesn't change if wrong action taken" do
-    stages = Stages.new
-    result = Stages.check(stages, :completely_wrong_event)
+  # test "Stage doesn't change if wrong action taken" do
+  #   stages = Stages.new
+  #   result = Stages.check(stages, :completely_wrong_event)
 
-    assert result == :error
-    assert stages.stage == :adding_players
-  end
+  #   assert result == :error
+  #   assert stages.stage == :initial_setup
+  # end
 
-  describe "Adding Players" do
-    test "Adding a new player doesn't change stage" do
-      stages = Stages.new
+  # describe "Adding Players" do
+  #   test "Adding a new player doesn't change stage" do
+  #     stages = Stages.new
 
-      assert {:ok, stages} = Stages.check(stages, :add_player)
-      assert stages.stage == :adding_players
-    end
+  #     assert {:ok, stages} = Stages.check(stages, :add_player)
+  #     assert stages.stage == :adding_players
+  #   end
 
-    test "Can't add player in other states" do
-      stages = Stages.new
-      stages = %{stages | stage: :some_other_stage}
+  #   test "Can't add player in other states" do
+  #     stages = Stages.new
+  #     stages = %{stages | stage: :some_other_stage}
 
-      assert :error = Stages.check(stages, :add_player)
-    end
+  #     assert :error = Stages.check(stages, :add_player)
+  #   end
 
-    # TODO next
-    # result = Stages.check(stages, {:start_game, number_of_players})
-  end
+  #   # TODO next
+  #   # result = Stages.check(stages, {:start_game, number_of_players})
+  # end
 end
