71c174d694177ca7ca824973fb212a828cb1844e
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-25 00:21:12 -0400

    Start to brainstorm the shape of Game and it's returns

--- test/ink_flier/game_test.exs
+++ test/ink_flier/game_test.exs
@@ -2,4 +2,35 @@ defmodule InkFlierTest.Game do
   use ExUnit.Case
 
   alias InkFlier.Game
+  # new
+  #   - HouseRules
+  # add players
+  # select track
+  # begin
+  #   - player starting positions
+  # *broadcast:
+  #   %PlayerInfo?{
+  #     player_1: %PlayerInfo{
+  #       current_position: {1,1}
+  #     }
+  #     crash return?
+  #     disconected return?
+  #
+  #
+  #
+  #
+  #     current_positions:
+  #     legal_moves: %{
+  #       player_1: MapSet.new([{1,1}, {1,2}...]),
+  #       player_2: MapSet.new([{1,1}, {1,2}...]),
+  #     }
+  #   }
+  # move
+
+  # try things in wrong phase and confirm error
+  # move events:
+  # - lock_in_move
+  # - leave_game
+  # - win
+  # - crash?
 end
