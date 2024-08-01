a0c7fc6a845a288d0740bed20177971d40f1c424
Eric Ridgeway <ericridgeway@gmail.com>
2024-07-31 20:13:06 -0400

    Rename/extract coordToCar (and vice versa) tuples
    Also add a comment to finish the Enum.random playersInOrder function in the future

--- lib/ink_flier/board.ex
+++ lib/ink_flier/board.ex
@@ -8,21 +8,22 @@ defmodule InkFlier.Board do
     players
     |> players_in_order(random_pole_position?)
     |> Enum.zip(track_start_coords)
-    |> Enum.map(&player_and_car_tuple/1)
+    |> Enum.map(&coord_to_car_tuple/1)
     |> Map.new
   end
 
   def current_positions(t) do
     t
-    |> Enum.map(fn {player, car} ->
-      {player, Car.position(car)}
-    end)
+    |> Enum.map(&car_to_coord_tuple/1)
     |> Map.new
   end
 
 
-  defp player_and_car_tuple({player, coord}), do: {player, Car.new(coord)}
+  defp coord_to_car_tuple({player, coord}), do: {player, Car.new(coord)}
+  defp car_to_coord_tuple({player, car}), do: {player, Car.position(car)}
 
   defp players_in_order(players, random_pole_position?)
+  # TODO
+  # defp players_in_order(players, true), do: ...
   defp players_in_order(players, false), do: players
 end
