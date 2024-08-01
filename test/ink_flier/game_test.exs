585e2527fe547ad8fe0f5ae04df54fd79daef395
Eric Ridgeway <ericridgeway@gmail.com>
2024-08-01 01:20:10 -0400

    Brainstorm many of the main Game.move tests
    Leave next test failing for todo-next point

--- test/ink_flier/game_test.exs
+++ test/ink_flier/game_test.exs
@@ -9,8 +9,28 @@ defmodule InkFlierTest.Game do
     assert_receive {:starting_positions, %{a: {-1,-1}, b: {-2,-2}}}
   end
 
-  # test "move" do
-  # end
+  describe "move" do
+    test "Move once locks in" do
+      {:ok, pid} = Game.start_link([:a, :b], Helpers.test_track, self())
+
+      assert :ok = Game.move(pid, :a, {0,-1})
+      assert_receive {:player_locked_in, :a}
+    end
+
+    # Both players moved = next round
+    #   - Send all current_positions
+
+    # test "legal_move?" do
+    # # NOTE extra interface function, yes. For drawing the x,y places you're allowed to hover over. Like the chess board
+    # end
+
+    # illegal move
+    # move results in crash
+    # move results in win (after everyone locked in)
+    #   - multiple winners possible
+
+    # Can't move again until all locked in
+  end
 
   # test "resign" do
   # end
