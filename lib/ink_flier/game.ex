85bae375a7390439cc9b97689e150efee6b27d3f
Eric Ridgeway <ericridgeway@gmail.com>
2024-08-04 20:12:11 -0400

    Fully seperate RoundTracker from Game
    RoundTracker now owns currentRound, who's-locked-in, and even the full list of Players
    
    Also reorder a lot of the "getter and setter" functions. Let's try to group them all at the bottom (defp or not), with (more or less) getters, then deep-getters, then setters, updaters... something like that

--- lib/ink_flier/game.ex
+++ lib/ink_flier/game.ex
@@ -9,16 +9,9 @@ defmodule InkFlier.Game do
   alias InkFlier.RaceTrack
   alias InkFlier.RoundTracker
 
-  @typedoc "Given order is maintained, although currently only used for prioritizing starting positions"
-  @type players :: [Game.player_id]
-
-  @type player_id :: any
-
   typedstruct enforce: true do
     field :board, Board.t
-    field :players, players
     field :round_tracker, RoundTracker.t
-    field :locked_in, MapSet.t(player_id), default: MapSet.new
     field :notify_target, Server.notify_target, required: false
     field :track, RaceTrack.t
     # field :house_rules
@@ -28,17 +21,16 @@ defmodule InkFlier.Game do
     track_start_coords = RaceTrack.start(track)
     random_pole_position? = false
     board = Board.new(players, track_start_coords, random_pole_position?)
-    round_tracker = RoundTracker.new
+    round_tracker = RoundTracker.new(players)
 
-    struct!(__MODULE__, ~M{board, track, notify_target, players, round_tracker})
+    struct!(__MODULE__, ~M{board, round_tracker, track, notify_target})
   end
   def new(players, track, notify_target \\ nil), do: new(~M{players, track, notify_target})
 
   def move(t, player, coord) do
     t
     |> do_move(player, coord)
-    |> lock_in(player)
-    |> maybe_advance_round
+    |> lock_in_player_for_round(player)
   end
 
   def check_legal_move(t, player, coord) do
@@ -49,39 +41,22 @@ defmodule InkFlier.Game do
     unless locked_in?(t, player), do: :ok, else: {:error, :already_locked_in}
   end
 
+  defp car(t, player), do: get_in(t, car_key(player))
+
+  defp car_key(player), do: [key(:board), key(player)]
+
+
   def board(t), do: t.board
   def notify_target(t), do: t.notify_target
-  def locked_in(t), do: t.locked_in
-  def players(t), do: t.players
   def round_tracker(t), do: t.round_tracker
 
-  def current_round(t), do: t |> round_tracker |> RoundTracker.current
   def current_positions(t), do: t |> board |> Board.current_positions
-
+  def current_round(t), do: t |> round_tracker |> RoundTracker.current
+  defp locked_in?(t, player), do: t |> round_tracker |> RoundTracker.locked_in?(player)
   def speed(t, player), do: t |> car(player) |> Car.speed
   def legal_move?(t, player, coord), do: t |> car(player) |> Car.legal_move?(coord)
 
-
-  defp maybe_advance_round(t) do
-    if MapSet.equal?(players_set(t), locked_in(t)), do: advance_round(t), else: t
-  end
-
-  defp players_set(t), do: t |> players |> MapSet.new
-
-  defp car(t, player), do: get_in(t, car_key(player))
-
-  defp advance_round(t) do
-    t
-    |> Map.update!(:round_tracker, &RoundTracker.advance/1)
-    |> reset_locked_in
-  end
-
-  defp reset_locked_in(t), do: put_in(t.locked_in, MapSet.new)
-  defp lock_in(t, player), do: update_in(t.locked_in, &MapSet.put(&1, player))
-
   defp do_move(t, player, coord), do: update_in(t, car_key(player), &Car.move(&1, coord))
-
-  defp locked_in?(t, player), do: player in locked_in(t)
-
-  defp car_key(player), do: [key(:board), key(player)]
+  defp lock_in_player_for_round(t, player), do:
+    update_in(t.round_tracker, &RoundTracker.player_moved(&1, player))
 end
