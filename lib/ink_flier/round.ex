6275d49fff68a9d55234ca648a06c85357ce972a
Eric Ridgeway <ericridgeway@gmail.com>
2024-08-08 03:22:43 -0400

    Start designing Round
    At this point, realize I need Board to store list of Players in order

--- lib/ink_flier/round.ex
+++ lib/ink_flier/round.ex
@@ -1,3 +1,19 @@
 defmodule InkFlier.Round do
+  alias InkFlier.Board
+  alias InkFlier.Player
+  alias InkFlier.RaceTrack
 
+  @type t :: :"TODO this t just becomes typedstruct prob"
+  @type reply :: {t, [instruction]}
+  @type instruction :: {:notify_room, room_instruction}
+  @type room_instruction ::
+      {:new_round, integer}
+      | {:player_position, Game.player_id, %{coord: Coord.t, speed: integer}}
+
+
+  # @spec new(integer, Board.t, RaceTrack.t) :: reply
+  # def new(round_number, current_board, track) do
+  #   instructions = [{:notify_room, {:new_round, round_number}}]
+
+  # end
 end
--- test/ink_flier/round_test.exs
+++ test/ink_flier/round_test.exs
@@ -1,5 +1,23 @@
 defmodule InkFlierTest.Round do
   use ExUnit.Case
+  import TinyMaps
 
+  alias InkFlierTest.Helpers
   alias InkFlier.Round
+
+  # setup do
+  #   round = Round.new(1, [:a, :b], Helpers.test_track)
+  #   ~M{round}
+  # end
+
+  # test "new" do
+  #   board = Board.new([:a, :b], [{-1,-1}, {-2,-2}, {-3,-3}, {-4,-4}])
+
+  #   assert {_round, instructions} = Round.new(1, board, Helpers.test_track)
+  #   assert instructions == [
+  #     {:notify_room, {:new_round, 1}},
+  #     {:notify_room, {:player_position, :a, %{coord: {-1,-1}, speed: 1}}},
+  #     {:notify_room, {:player_position, :b, %{coord: {-2,-2}, speed: 1}}},
+  #   ]
+  # end
 end
