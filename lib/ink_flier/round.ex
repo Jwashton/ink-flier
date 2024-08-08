95913b1706dae80f98454b6dc84184e7b634feb5
Eric Ridgeway <ericridgeway@gmail.com>
2024-08-08 17:53:25 -0400

    Add type and doc to Round

--- lib/ink_flier/round.ex
+++ lib/ink_flier/round.ex
@@ -1,20 +1,37 @@
 defmodule InkFlier.Round do
+  @moduledoc """
+  Abstraction for a single round of the game
+
+  It tracks who has & hasn't locked in, and manages moves on the new board for this round
+
+  Produces lists of instructions for notifying players (or the entire room) of events
+  like "a player locked in" or "the round ended"
+  """
   use TypedStruct
 
   alias InkFlier.Game
   alias InkFlier.Board
 
   @type reply :: {t, [instruction]}
-  @type instruction :: {:notify_room, room_instruction}
+
+  @type instruction ::
+      {:notify_room, room_instruction} |
+      {:notify_player, Game.player_id, player_instruction}
+
   @type room_instruction ::
-      {:new_round, integer}
-      | {:player_position, Game.player_id, %{coord: Coord.t, speed: integer}}
+      {:new_round, integer} |
+      {:player_position, Game.player_id, %{coord: Coord.t, speed: integer}} |
+      {:player_locked_in, Game.player_id}
+
+  @type player_instruction ::
+      {:speed, integer}
 
   typedstruct enforce: true do
     field :board, Board.t
     field :locked_in, MapSet.t(Game.player_id), default: MapSet.new
   end
 
+  @doc "Build a new round and initial notification instructions"
   @spec new(Board.t, integer) :: reply
   def new(current_board, round_number) do
     t = struct!(__MODULE__, board: current_board)
@@ -34,6 +51,19 @@ defmodule InkFlier.Round do
   @doc false
   def move({t, _previous_instructions}, player, destination), do: move(t, player, destination)
 
+  @doc """
+  Attempt to move a player in the current round
+
+  This is the main function of the game
+
+  Many replies to an attempted move are possible. They are listed in the `t:reply/0` type.
+  Some examples:
+  - "Normal move that was sucessful"
+  - "Normal move by final player, so round ends"
+  - "Illegal move, attempted to move too far"
+  - "Illegal move, this player already locked in"
+  """
+  @spec move(t, Game.player_id, Coord.t) :: reply
   def move(t, player, destination) do
     t =
       t
