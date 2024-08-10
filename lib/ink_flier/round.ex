c57f502192595ed18e05fd714da1d54f09877013
Eric Ridgeway <ericridgeway@gmail.com>
2024-08-09 23:45:15 -0400

    Abstract the {Round, RoundInstructions} tuple

--- lib/ink_flier/round.ex
+++ lib/ink_flier/round.ex
@@ -1,4 +1,29 @@
 defmodule InkFlier.Round do
+  defmodule Reply do
+    @type t :: {Round.t, [Round.instruction]}
+
+    @spec new(Round.t) :: t
+    def new(round), do: {round, []}
+
+    @spec round(t, (Round.t -> Round.t)) :: t
+    def round({round, _} = t, round_updater) do
+      round
+      |> round_updater.()
+      |> then(& put_elem(t, 0, &1) )
+    end
+
+    @spec instruction(t, (Round.t -> Round.instruction) ) :: t
+    def instruction({round, _instructions} = t, new_instruction_func) when is_function(new_instruction_func) do
+      instruction(t, new_instruction_func.(round))
+    end
+
+    @spec instruction(t, Round.instruction) :: t
+    def instruction({_round, instructions} = t, new_instruction) do
+      instructions ++ [new_instruction]
+      |> then(& put_elem(t, 1, &1) )
+    end
+  end
+
   @moduledoc """
   Abstraction for a single round of the game
 
@@ -11,8 +36,6 @@ defmodule InkFlier.Round do
 
   alias InkFlier.Game
   alias InkFlier.Board
-
-  @type reply :: {t, [instruction]}
 
   @type instruction ::
       {:notify_room, room_instruction} |
@@ -33,7 +56,7 @@ defmodule InkFlier.Round do
   end
 
   @doc "Build a new round and initial notification instructions"
-  @spec new(Board.t, integer) :: reply
+  @spec new(Board.t, integer) :: Reply.t
   def new(current_board, round_number) do
     t = struct!(__MODULE__, board: current_board)
 
@@ -57,28 +80,22 @@ defmodule InkFlier.Round do
 
   This is the main function of the game
 
-  Many replies to an attempted move are possible. They are listed in the `t:reply/0` type.
+  Many responses to an attempted move are possible. They are listed in the `t:instruction/0` type.
   Some examples:
   - "Normal move that was sucessful"
   - "Normal move by final player, so round ends"
   - "Illegal move, attempted to move too far"
   - "Illegal move, this player already locked in"
   """
-  @spec move(t, Game.player_id, Coord.t) :: reply
+  @spec move(t, Game.player_id, Coord.t) :: Reply.t
   def move(t, player, destination) do
     with :ok <- check_legal_move(t, player, destination) do
-      t =
-        t
-        |> do_move(player, destination)
-        |> lock_in(player)
-
-      instructions =
-        [
-          {:notify_room, {:player_locked_in, player}},
-          {:notify_player, player, {:speed, speed(t, player)}}
-        ]
-
-      {t, instructions}
+      t
+      |> Reply.new
+      |> Reply.round(&do_move(&1, player, destination))
+      |> Reply.round(&lock_in(&1, player))
+      |> Reply.instruction({:notify_room, {:player_locked_in, player}})
+      |> Reply.instruction(&{:notify_player, player, {:speed, speed(&1, player)}})
     else
       error -> {t, [{:notify_player, player, error}]}
     end
