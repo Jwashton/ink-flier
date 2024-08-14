156abb53c036970336fbc0573560db93c83315fb
Eric Ridgeway <ericridgeway@gmail.com>
2024-08-14 02:31:55 -0400

    Extract @start_coords var in board test

--- test/ink_flier/board_test.exs
+++ test/ink_flier/board_test.exs
@@ -3,19 +3,21 @@ defmodule InkFlierTest.Board do
 
   alias InkFlier.Board
 
+  @start_coords [{-1,-1}, {-2,-2}, {-3,-3}, {-4,-4}]
+
   describe "Can make new Board with and without random pole positions" do
     test "Using given pole positions" do
-      t = Board.new([:a, :b, :c], [{-1,-1}, {-2,-2}, {-3,-3}, {-4,-4}])
+      t = Board.new([:a, :b, :c], @start_coords)
       assert t |> Board.current_positions == %{a: {-1,-1}, b: {-2,-2}, c: {-3,-3}}
     end
 
     test "Using random pole positions" do
-      t = Board.new([:a, :b, :c], [{-1,-1}, {-2,-2}, {-3,-3}, {-4,-4}], true, &Enum.reverse/1)
+      t = Board.new([:a, :b, :c], @start_coords, true, &Enum.reverse/1)
       assert t |> Board.current_positions == %{c: {-1,-1}, b: {-2,-2}, a: {-3,-3}}
     end
 
     test "Using ACTUAL random pole positions (no module override) doesn't error" do
-      t =Board.new([:a, :b, :c], [{-1,-1}, {-2,-2}, {-3,-3}, {-4,-4}], true)
+      t =Board.new([:a, :b, :c], @start_coords, true)
       for player <- [:a, :b, :c] do
         assert t |> Board.current_positions |> Map.has_key?(player)
       end
@@ -23,7 +25,7 @@ defmodule InkFlierTest.Board do
   end
 
   test "Board maintains list of player order" do
-    t = Board.new([:b, :z, :a], [{-1,-1}, {-2,-2}, {-3,-3}, {-4,-4}])
+    t = Board.new([:b, :z, :a], @start_coords)
     assert t |> Board.players == [:b, :z, :a]
   end
 end
