e00c5723fe19aabbf0bb570350047a27450d436d
Eric Ridgeway <ericridgeway@gmail.com>
2024-09-01 04:24:18 -0400

    Add some example doc to instructions, altho there's a todo note to pause this until I rewrite the return type of these instruction lists to something else

--- lib/ink_flier/round/instruction.ex
+++ lib/ink_flier/round/instruction.ex
@@ -2,6 +2,20 @@ defmodule InkFlier.Round.Instruction do
   alias InkFlier.Round
   alias InkFlier.Round.Reply
 
+  @doc """
+  Instructions after locking in a player
+
+  ## Examples
+      iex> {_round, instructions} =
+      ...> Round.new(board, round_number) |>
+      ...> Round.move(:player_1, valid_destination)
+      iex> instructions
+      [
+        {:notify_room, {:player_locked_in, :player_1}},
+        {:notify_player, :player_1, {:ok, {:speed, 1}}}
+      ]
+
+  """
   def player_locked_in(reply, player) do
     reply
     |> Reply.add_instruction({:notify_room, {:player_locked_in, player}})
