09d2a2235b8f55e2bacf72a87252287db329bae3
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-04 02:32:05 -0400

    Start adding form submission handler for login

--- lib/ink_flier_web/controllers/login_controller.ex
+++ lib/ink_flier_web/controllers/login_controller.ex
@@ -2,6 +2,17 @@ defmodule InkFlierWeb.LoginController do
   use InkFlierWeb, :controller
 
   def new(conn, _params) do
-    render(conn, :new, layout: false)
+    conn
+    |> assign_user
+    |> render(:new, layout: false)
   end
+
+  def create(conn, _params) do
+    conn
+    |> assign_user
+    |> render(:new, layout: false)
+  end
+
+
+  defp assign_user(conn), do: assign(conn, :user, :TODO)
 end
