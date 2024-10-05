5abcce7cf42c36bd771c7c46c02d0332fd2a0996
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-05 02:03:25 -0400

    For revert, first attempt to have login partial redirect to OUTER page, not itself the inner login.html

--- lib/ink_flier_web/controllers/login_controller.ex
+++ lib/ink_flier_web/controllers/login_controller.ex
@@ -2,7 +2,11 @@ defmodule InkFlierWeb.LoginController do
   use InkFlierWeb, :controller
 
   def new(conn, _params) do
+    conn.assigns
+    |> IO.inspect(charlists: :as_lists, label: "assignaaaaaaaaaaaaaaaaaaaaaaaaas")
+
     conn
+    |> assign(:dbg_path, current_path(conn))
     |> render(:new, layout: false)
   end
 
