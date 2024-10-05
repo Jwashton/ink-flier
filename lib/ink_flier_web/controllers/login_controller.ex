7075a8aeb9aa021bc4e8ce4e5f2b1ac1d6cd784d
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-05 02:02:55 -0400

    Extract redirectToSelf function

--- lib/ink_flier_web/controllers/login_controller.ex
+++ lib/ink_flier_web/controllers/login_controller.ex
@@ -12,17 +12,22 @@ defmodule InkFlierWeb.LoginController do
     if user == "" do
       conn
       |> put_flash(:error, "Can't be blank")
-      |> redirect(to: ~p"/login")
+      |> redirect_to_self
     else
       conn
       |> put_session(:user, user)
-      |> redirect(to: ~p"/login")
+      |> redirect_to_self
     end
   end
 
   def delete(conn, _params) do
     conn
     |> delete_session(:user)
-    |> redirect(to: ~p"/login")
+    |> redirect_to_self
+  end
+
+
+  defp redirect_to_self(conn) do
+    redirect(conn, to: conn.assigns.current_path)
   end
 end
