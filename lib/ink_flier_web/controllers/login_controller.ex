277e2be61eb78d5c8c0c4d890e1008eebab92460
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-08 02:50:46 -0400

    Successfully turn login into a function component with an optional return_to param!

--- lib/ink_flier_web/controllers/login_controller.ex
+++ lib/ink_flier_web/controllers/login_controller.ex
@@ -1,33 +1,39 @@
 defmodule InkFlierWeb.LoginController do
   use InkFlierWeb, :controller
 
+  plug :default_return_to, "/login"
+
   def new(conn, _params) do
     conn
     |> render(:new, layout: false)
   end
 
-  def create(conn, %{"user_name" => raw_user} = _params) do
+  def create(conn, params) do
+    %{"user_name" => raw_user, "return_to" => return_to} = params
     user = String.trim(raw_user)
 
     if user == "" do
       conn
       |> put_flash(:error, "Can't be blank")
-      |> redirect_to_self
+      |> redirect(to: return_to)
     else
       conn
       |> put_session(:user, user)
-      |> redirect_to_self
+      |> redirect(to: return_to)
     end
   end
 
-  def delete(conn, _params) do
+  def delete(conn, params) do
+    %{"return_to" => return_to} = params
+
     conn
     |> delete_session(:user)
-    |> redirect_to_self
+    |> redirect(to: return_to)
   end
 
 
-  defp redirect_to_self(conn) do
-    redirect(conn, to: ~p"/lobby")
+  defp default_return_to(conn, default_return_to) do
+    return_to = conn.assigns[:return_to] || default_return_to
+    assign(conn, :return_to, return_to)
   end
 end
