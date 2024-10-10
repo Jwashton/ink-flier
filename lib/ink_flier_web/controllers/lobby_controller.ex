293a9b69e842e8d5cb29c1814594d830ae1e308b
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-10 01:30:00 -0400

    Delete old original placeholder attempt at username/playerId

--- lib/ink_flier_web/controllers/lobby_controller.ex
+++ lib/ink_flier_web/controllers/lobby_controller.ex
@@ -3,12 +3,6 @@ defmodule InkFlierWeb.LobbyController do
 
   def home(conn, _params) do
     conn
-    |> assign_player_id
     |> render(:home, layout: false)
-  end
-
-
-  defp assign_player_id(conn) do
-    assign(conn, :player_id, :TODO)
   end
 end
