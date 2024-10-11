7c25b382369f2d857a639e9e7b8305e86de3e536
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-11 03:45:35 -0400

    Add next todo note, need to remember how this supervisor of little game processes works

--- test/ink_flier/lobby_test.exs
+++ test/ink_flier/lobby_test.exs
@@ -4,6 +4,13 @@ defmodule InkFlierTest.Lobby do
   alias InkFlier.Lobby
 
   test "Initial state" do
+    # Ecto.UUID.generate
+    # |> IO.inspect(charlists: :as_lists, label: "uuid")
+
     assert Lobby.games == %{}
+  end
+
+  test "Create game" do
+    raise "NEXT This is supervisor stuff right? Games will be processes (just use spawn for now for testing) that will need to be supervised by this lobby. Is there a special module I read about in the Elixir book when a module's job is just to start and track processes? Where it keeps their pids and updates them if the process crashes?"
   end
 end
