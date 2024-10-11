ac79d7fb5e63cb2c59e1a433f13dcdc4f0deb42b
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-11 19:42:22 -0400

    Sketch out some of the test for Lobby

--- lib/ink_flier/game.ex
+++ lib/ink_flier/game.ex
@@ -3,4 +3,18 @@ defmodule InkFlier.Game do
   @type player_id :: any
   @type observer_id :: any
   @type member_id :: player_id | observer_id
+
+  use Agent
+
+  def start_link(initial_value) do
+    Agent.start_link(fn -> initial_value end)
+  end
+
+  def value(pid) do
+    Agent.get(pid, & &1)
+  end
+
+  def increment(pid) do
+    Agent.update(pid, &(&1 + 1))
+  end
 end
--- test/ink_flier/lobby_test.exs
+++ test/ink_flier/lobby_test.exs
@@ -2,6 +2,7 @@ defmodule InkFlierTest.Lobby do
   use ExUnit.Case
 
   alias InkFlier.Lobby.Server
+  alias InkFlier.Game
 
   test "Initial state" do
     # Ecto.UUID.generate
@@ -13,6 +14,10 @@ defmodule InkFlierTest.Lobby do
   test "Create game" do
     # raise "NEXT This is supervisor stuff right? Games will be processes (just use spawn for now for testing) that will need to be supervised by this lobby. Is there a special module I read about in the Elixir book when a module's job is just to start and track processes? Where it keeps their pids and updates them if the process crashes?"
 
-    # game1 = spawn
+    # {:ok, game1_pid} = Game.start_link(:whatever_game_data)
+    {:ok, game_id} = Server.create_game(:imaginary_game1)
+
+    assert game_id in Server.games |> Map.keys
+    assert Server.get(game_id) == :imaginary_game1
   end
 end
