464ec50491bd1d9b7fc0cf1f8fb41f7b2bc26980
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-12 00:19:42 -0400

    For revert. Let's make game first then just track it in lobby. Don't have lobby try to make the game at the same time as assigning it an id

--- lib/ink_flier/lobby.ex
+++ lib/ink_flier/lobby.ex
@@ -6,6 +6,7 @@ defmodule InkFlier.Lobby do
   typedstruct enforce: true do
     field :next_game_id, game_id, default: 1
     field :games, games, default: %{}
+    field :game_module, any, default: Game
   end
 
   @type game_id :: integer
@@ -13,6 +14,22 @@ defmodule InkFlier.Lobby do
 
   def new, do: struct!(__MODULE__)
 
+  def create_game(t, track) do
+    game = t.game_module.new(track)
+    game_id = t.next_game_id
+
+    t =
+      t
+      |> update_games(&Map.put(&1, game_id, game))
+      |> inc_next_game_id
+
+    {t, game_id}
+  end
+
 
   def games(t), do: t.games
+
+  def set_game_module(t, module), do: put_in(t.game_module, module)
+  def update_games(t, func), do: update_in(t.games, func)
+  def inc_next_game_id(t), do: update_in(t.next_game_id, & &1 + 1)
 end
--- test/ink_flier/lobby_test.exs
+++ test/ink_flier/lobby_test.exs
@@ -4,6 +4,17 @@ defmodule InkFlierTest.Lobby do
   alias InkFlier.Lobby.Server
   alias InkFlier.Game
 
+  defmodule MockGame do
+    def new(track) do
+      nil
+    end
+
+    def track(t) do
+      :hi_imaginary_game1
+    end
+  end
+
+
   test "Initial state" do
     # Ecto.UUID.generate
     # |> IO.inspect(charlists: :as_lists, label: "uuid")
@@ -15,9 +26,14 @@ defmodule InkFlierTest.Lobby do
     # raise "NEXT This is supervisor stuff right? Games will be processes (just use spawn for now for testing) that will need to be supervised by this lobby. Is there a special module I read about in the Elixir book when a module's job is just to start and track processes? Where it keeps their pids and updates them if the process crashes?"
 
     # {:ok, game1_pid} = Game.start_link(:whatever_game_data)
-    {:ok, game_id} = Server.create_game(:imaginary_game1)
+    :ok = Server.set_game_module(MockGame)
+    {:ok, game_id} = Server.create_game(:imaginary_track_number)
 
     assert game_id in Server.games |> Map.keys
-    assert Server.get(game_id) == :imaginary_game1
+    # assert Server.get(game_id) |> Game.track == :imaginary_game1
   end
+
+
+  # TODO note, the tests might bleed together since Lobby is a process, and it's started in the Application file. Possible the tests don't reset it for every test. Check later
+
 end
