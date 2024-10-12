9543ea8ce9647a1636438704c373bbe39f991695
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-12 02:48:12 -0400

    Rewrite tests to confirm application DOES start the lobby server

--- test/ink_flier/lobby_server_test.exs
+++ test/ink_flier/lobby_server_test.exs
@@ -3,24 +3,33 @@ defmodule InkFlierTest.LobbyServer do
 
   alias InkFlier.LobbyServer
 
-  setup do
-    # Application auto-starts this Server. Let's manually restart it between each test
-    # so they don't "bleed" into eachother
+  test "Confirm server starts in application" do
+    assert LobbyServer.games == %{}
+  end
+
+  describe "Restart server between tests" do
+    setup [:restart_server]
+
+    test "Add game" do
+      {:ok, game_id1} = LobbyServer.add_game(:fake_game1)
+      {:ok, game_id2} = LobbyServer.add_game(:fake_game2)
+      games = LobbyServer.games
+
+      assert games[game_id1] == :fake_game1
+      assert games[game_id2] == :fake_game2
+    end
+
+    test "Confirm tests don't bleed" do
+      assert LobbyServer.games == %{}
+    end
+  end
+
+
+  # Application auto-starts this Server. Let's manually restart it between each test
+  # so they don't "bleed" into eachother
+  defp restart_server(_) do
     LobbyServer.stop
     LobbyServer.start_link(nil)
     :ok
-  end
-
-  test "Add game" do
-    {:ok, game_id1} = LobbyServer.add_game(:fake_game1)
-    {:ok, game_id2} = LobbyServer.add_game(:fake_game2)
-    games = LobbyServer.games
-
-    assert games[game_id1] == :fake_game1
-    assert games[game_id2] == :fake_game2
-  end
-
-  test "Confirm tests don't bleed" do
-    assert LobbyServer.games == %{}
   end
 end
