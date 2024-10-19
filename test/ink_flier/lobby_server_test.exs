f04e878dac77fcafbc9017c14b3a80983684d65d
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-18 23:50:39 -0400

    Finish fixing intermittently failing LobbyServer tests

--- test/ink_flier/lobby_server_test.exs
+++ test/ink_flier/lobby_server_test.exs
@@ -4,6 +4,7 @@ defmodule InkFlierTest.LobbyServer do
   alias InkFlier.LobbyServer
 
   test "Confirm server starts in application" do
+    # DON'T call with Testlobby name, we're checking if global application LobbyServer started here
     assert LobbyServer.games == %{}
   end
 
@@ -11,26 +12,24 @@ defmodule InkFlierTest.LobbyServer do
     setup [:restart_server]
 
     test "Add game" do
-      {:ok, game_id1} = LobbyServer.add_game(:fake_game1)
-      {:ok, game_id2} = LobbyServer.add_game(:fake_game2)
-      games = LobbyServer.games
+      {:ok, game_id1} = LobbyServer.add_game(TestLobby, :fake_game1)
+      {:ok, game_id2} = LobbyServer.add_game(TestLobby, :fake_game2)
+      games = LobbyServer.games(TestLobby)
 
       assert games[game_id1] == :fake_game1
       assert games[game_id2] == :fake_game2
     end
 
     test "Confirm tests don't bleed" do
-      assert LobbyServer.games == %{}
+      assert LobbyServer.games(TestLobby) == %{}
     end
   end
 
 
-  # Application auto-starts this Server. Let's manually restart it between each test
-  # so they don't "bleed" into eachother
+  # Application auto-starts this Server. Let's manually restart a non-application one
+  # between each test so they don't "bleed" into eachother
   defp restart_server(_) do
-    LobbyServer.stop
-    :timer.sleep(200)
-    LobbyServer.start_link([])
+    LobbyServer.start_link([name: TestLobby])
     :ok
   end
 end
