af5ae93419799021048bec7330f510526918a541
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-19 23:02:10 -0400

    Remove join param from url after using it

--- lib/ink_flier_web/controllers/lobby_game_controller.ex
+++ lib/ink_flier_web/controllers/lobby_game_controller.ex
@@ -4,12 +4,17 @@ defmodule InkFlierWeb.LobbyGameController do
 
   alias InkFlier.GameServer
 
-  def home(conn, ~m{game_id, _join}) do
+  def home(conn, ~m{game_id, _join} = params) do
+    params =
+      params
+      |> Map.delete("join")
+      |> Map.delete("game_id")
+
     case GameServer.join(game_id, conn.assigns.user) do
       :ok -> conn
       {:error, _} = error -> put_flash(conn, :error, inspect(error))
     end
-    |> home(~m{game_id})
+    |> redirect(to: current_path(conn, params))
   end
 
   def home(conn, ~m{game_id}) do
@@ -28,5 +33,4 @@ defmodule InkFlierWeb.LobbyGameController do
   #   |> put_flash(:error, "Missing game id in url, eg. lobby/game/28")
   #   |> redirect(to: ~p"/lobby")
   # end
-
 end
