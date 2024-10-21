da1c0163a34a5d0cd8563e55352ed7dfa7620175
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-20 20:39:38 -0400

    Add starter page js for game channel page

---
+++ assets/js/game_socket.js
@@ -0,0 +1,14 @@
+import {Socket} from "phoenix"
+
+
+let socket = new Socket("/socket", {params: {token: window.userToken}})
+socket.connect()
+
+let channel = socket.channel("room:lobby", {})
+channel.join()
+  .receive("ok", resp => {
+  })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+
+
+export default socket
