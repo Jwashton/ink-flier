7a723d677aa03ffd366d3dedd3a287e333a48eb2
Eric Ridgeway <ericridgeway@gmail.com>
2024-10-29 12:29:45 -0400

    Don't start unique GameSupervisor in tests
    Aparently I can use Application-started one and get away with only force restarting the top called, LobbyServer, between tests

--- test/ink_flier/lobby_server_test.exs
+++ test/ink_flier/lobby_server_test.exs
@@ -2,15 +2,17 @@ defmodule InkFlierTest.LobbyServer do
   use ExUnit.Case
 
   alias InkFlier.LobbyServer
-  alias InkFlier.GameSupervisor
   alias InkFlier.GameServer
 
   @lobby __MODULE__.LobbyServer
-  @game_starter __MODULE__.GameSupervisor
 
   setup do
-    start_supervised!({GameSupervisor, name: @game_starter})
-    start_supervised!({LobbyServer, name: @lobby, game_supervisor: @game_starter})
+    # NOTE Aparently I don't need unique GameSupervisor for tests to not collide? I can just use
+    # the Application-started one
+
+    # start_supervised!({GameSupervisor, name: @game_starter})
+    # start_supervised!({LobbyServer, name: @lobby, game_supervisor: @game_starter})
+    start_supervised!({LobbyServer, name: @lobby})
     :ok
   end
 
