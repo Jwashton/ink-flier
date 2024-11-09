49f5de22112aa5447841f7b35210fd455562257a
Eric Ridgeway <ericridgeway@gmail.com>
2024-11-09 18:40:39 -0500

    Extract "login" setup in gameControllerTest

--- test/ink_flier_web/controllers/game_controller_test.exs
+++ test/ink_flier_web/controllers/game_controller_test.exs
@@ -2,24 +2,23 @@ defmodule InkFlierWeb.GameControllerTest do
   use InkFlierWeb.ConnCase
   import TinyMaps
 
+  describe "Login" do
+    setup [:login]
 
-  test "Page loads normally when game exists", ~M{conn} do
-    conn = init_test_session(conn, %{user: "Bob"})
+    test "Page loads normally when game exists", ~M{conn} do
+      start_supervised!(InkFlier.GameSystem)
+      {:ok, game_id} = InkFlier.LobbyServer.start_game(creator: "BillyBob")
 
-    start_supervised!(InkFlier.GameSystem)
-    {:ok, game_id} = InkFlier.LobbyServer.start_game(creator: "BillyBob")
+      conn = get(conn, ~p"/lobby/game/#{game_id}")
+      assert html_response(conn, 200) =~ "Game id: #{game_id}"
+    end
 
-    conn = get(conn, ~p"/lobby/game/#{game_id}")
-    assert html_response(conn, 200) =~ "Game id: #{game_id}"
+    test "If game doesn't exist, don't crash", ~M{conn} do
+      conn = get(conn, ~p"/lobby/game/badGameId")
+      assert html_response(conn, 200) =~ "does not exist"
+    end
   end
 
-  test "If game doesn't exist, don't crash", ~M{conn} do
-    conn = init_test_session(conn, %{user: "Bob"})
 
-    conn = get(conn, ~p"/lobby/game/badGameId")
-    assert html_response(conn, 200) =~ "does not exist"
-  end
-
+  defp login(~M{conn}), do: [conn: init_test_session(conn, %{user: "Bob"})]
 end
-
-# TODO extract setups, at least ":login"
