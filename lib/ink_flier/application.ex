9d84d0ad000b02566432e3f8925ed63bb2cb2511
Eric Ridgeway <ericridgeway@gmail.com>
2024-11-08 22:34:06 -0500

    Add conditional Application children starting based on test env

--- lib/ink_flier/application.ex
+++ lib/ink_flier/application.ex
@@ -7,32 +7,47 @@ defmodule InkFlier.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
+    children =
+      main_children()
+      ++ game_children()
+      ++ end_children()
+
+    opts = [strategy: :one_for_one, name: InkFlier.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    InkFlierWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+
+  def main_children do
+    [
       InkFlierWeb.Telemetry,
       InkFlier.Repo,
       {DNSCluster, query: Application.get_env(:ink_flier, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: InkFlier.PubSub},
       # Start the Finch HTTP client for sending emails
       {Finch, name: InkFlier.Finch},
-      # Start a worker by calling: InkFlier.Worker.start_link(arg)
-      # {InkFlier.Worker, arg},
-      # Start to serve requests, typically the last entry
       {Registry, keys: :unique, name: Registry.Game},
-      InkFlier.GameSystem,
-      InkFlierWeb.Endpoint
     ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: InkFlier.Supervisor]
-    Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  @impl true
-  def config_change(changed, _new, removed) do
-    InkFlierWeb.Endpoint.config_change(changed, removed)
-    :ok
+  def game_children do
+    if Application.get_env(:ink_flier, :supervise_games, true) do
+      [
+        InkFlier.GameSystem,
+      ]
+    else
+      []
+    end
+  end
+
+  def end_children do
+    [
+      InkFlierWeb.Endpoint
+    ]
   end
 end
