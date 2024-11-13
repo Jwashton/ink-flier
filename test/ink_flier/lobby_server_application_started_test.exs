19f49e985138c4e59b51750c6181aa112418db17
Eric Ridgeway <ericridgeway@gmail.com>
2024-11-12 23:01:50 -0500

    Make the first Integration test
    Confirming that, in the live, non-test environment, application DOES automatically start GameSystem like it should

--- test/ink_flier/lobby_server_application_started_test.exs
+++ test/ink_flier/lobby_server_application_started_test.exs
@@ -1,15 +1,16 @@
 defmodule InkFlierTest.LobbyServerApplicationStarted do
+  @moduledoc """
+  Special task to run these: `mix test.integration
+  """
   use ExUnit.Case
 
-  # alias InkFlier.LobbyServer
-  # alias InkFlier.GameServer
+  alias InkFlier.LobbyServer
+  alias InkFlier.GameServer
 
-
-  # test "Application correctly starts LobbyServer (and his required GameSupervisor)" do
-  #   Application.put_env(:ink_flier, :supervise_games, true)
-
-  #   assert LobbyServer.games == []
-  #   {:ok, game_id} = LobbyServer.start_game(creator: "Robin")
-  #   assert GameServer.creator(game_id) == "Robin"
-  # end
+  @tag integration: true
+  test "Application correctly starts LobbyServer (and his required GameSupervisor)" do
+    assert LobbyServer.games == []
+    {:ok, game_id} = LobbyServer.start_game(creator: "Robin")
+    assert GameServer.creator(game_id) == "Robin"
+  end
 end
