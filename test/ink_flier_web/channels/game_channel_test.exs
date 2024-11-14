e0118ff79757c8f6140e77b600a1234649f60076
Eric Ridgeway <ericridgeway@gmail.com>
2024-11-14 02:12:50 -0500

    Extract the "deals with both topics" tests to their own test file

--- test/ink_flier_web/channels/game_channel_test.exs
+++ test/ink_flier_web/channels/game_channel_test.exs
@@ -1,11 +1,10 @@
 defmodule InkFlierWeb.GameChannelTest do
   use InkFlierWeb.ChannelCase
   import TinyMaps
-  import InkFlierWebTest.ChannelSetup, only: [start_game: 1, join_lobby_channel: 1, join_game_channel: 1]
+  import InkFlierWebTest.ChannelSetup, only: [start_game: 1, join_game_channel: 1]
 
   alias InkFlier.LobbyServer
   alias InkFlier.GameServer
-  alias InkFlierWeb.LobbyChannel
 
   setup do
     start_supervised!(InkFlier.GameSystem)
@@ -26,19 +25,6 @@ defmodule InkFlierWeb.GameChannelTest do
       push!(game_socket, "leave", %{target: "badPlayer"})
 
       refute_broadcast("players_updated", _)
-    end
-  end
-
-
-  describe "Join both channels (lobby and game)" do
-    setup [:start_game, :join_lobby_channel, :join_game_channel]
-
-    test "Broadcast goes to multiple topics (Game AND Lobby)", ~M{game_topic, game_socket} do
-      lobby_topic = LobbyChannel.topic
-
-      push!(game_socket, "join", %{})
-      %{topic: ^game_topic} = assert_broadcast("players_updated", _)
-      %{topic: ^lobby_topic} = assert_broadcast("game_updated", _)
     end
   end
 
