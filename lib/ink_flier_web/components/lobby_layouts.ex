56803631d4311b9c00da8bba3c47e95daaa4bbe6
Eric Ridgeway <ericridgeway@gmail.com>
2024-11-18 00:14:59 -0500

    Refactor root layout slightly and add extra-styles option
    Similar to the just-finished extra-scripts option

--- lib/ink_flier_web/components/lobby_layouts.ex
+++ lib/ink_flier_web/components/lobby_layouts.ex
@@ -4,9 +4,16 @@ defmodule InkFlierWeb.LobbyLayouts do
   embed_templates "lobby_layouts/*"
 
   attr :scripts, :list
-  def scripts(assigns) do
+  def extra_scripts(assigns) do
     ~H"""
     <script :for={script <- @scripts || []} defer phx-track-static type="text/javascript" src={script}></script>
+    """
+  end
+
+  attr :styles, :list
+  def extra_styles(assigns) do
+    ~H"""
+    <link :for={style <- @styles || []} phx-track-static rel="stylesheet" href={style} />
     """
   end
 end
