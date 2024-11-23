cb36ae59939a8b8b08d14a33f3655cbd99dce371
William Ashton <William@AshtonFam.org>
2024-11-23 23:01:10 +0000

    Manage the DB host in an environment var

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :ink_flier, InkFlier.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "db",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   database: "ink_flier_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
