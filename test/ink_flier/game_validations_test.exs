a69fd6337f6ab3ee3d0ffe8107361498e1cbda16
Eric Ridgeway <ericridgeway@gmail.com>
2024-11-28 02:31:26 -0500

    Extract test module GameValidations

---
+++ test/ink_flier/game_validations_test.exs
@@ -0,0 +1,25 @@
+defmodule InkFlierTest.GameValidations do
+  use ExUnit.Case
+
+  alias InkFlier.Game
+
+  test "add_player/2" do
+    game        = Game.new
+    {:ok, game} = Game.add_player(game, "James")
+    reply       = Game.add_player(game, "James")
+
+    assert reply == {:error, :player_already_in_game}
+  end
+
+  test "remove_player/2" do
+    game        = Game.add_player!(Game.new, "James")
+    {:ok, game} = Game.remove_player(game, "James")
+    reply       = Game.remove_player(game, "James")
+
+    assert reply == {:error, :no_such_player}
+  end
+
+  test "start/1" do
+    assert Game.new |> Game.start == {:error, :requires_atleast_one_player}
+  end
+end
